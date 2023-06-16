62e23fb34d0eace52cf11ef50f0f1f7c195d51bf
zahir.iqbal97 <zahir.iqbal97.zahir.mohdiqbal@gmail.com>
2023-06-16 21:52:00 +0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBx7lbtX5ruPrR-41chbo4Q6UnHbUgVIZw",
+    authDomain: "netflix-clone-90a36.firebaseapp.com",
+    projectId: "netflix-clone-90a36",
+    storageBucket: "netflix-clone-90a36.appspot.com",
+    messagingSenderId: "255259989930",
+    appId: "1:255259989930:web:65e17c9b6251f606ce7dd3"
+};
